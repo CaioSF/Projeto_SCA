cb36f61916847b484b29e24be513f8f424ed3054
caioSF <caios8837@gmail.com>
2022-09-21 23:33:56 -0300

    Arquivos Imagens

--- sca/urls.py
+++ sca/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
-
-    path('', include('aplic.urls')),
     path('restrito/', admin.site.urls),
+    path('', include('aplic.urls')),
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
